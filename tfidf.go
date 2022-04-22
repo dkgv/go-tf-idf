39e294edcdb458cd486380aede96a19b76d9a997
Gustav <gustav-git@pm.me>
2022-04-22 09:13:33 +0200

    Fix coverage badge by actually writing coverprofile

--- tfidf.go
+++ tfidf.go
@@ -7,47 +7,6 @@ import (
 	"math"
 )
 
-type Document struct {
-	Frequency       map[string]float64
-	UniqueTokens    []string
-	TotalTokenCount int
-}
-
-func (d Document) TermFrequency(term string) float64 {
-	if _, ok := d.Frequency[term]; !ok {
-		return 0
-	}
-	return d.Frequency[term] / (float64)(d.TotalTokenCount)
-}
-
-func (d Document) GetVectors(other Document) ([]float64, []float64) {
-	visited := make(map[string]bool, 0)
-	terms := make([]string, 0)
-	for _, token := range d.UniqueTokens {
-		if _, ok := visited[token]; !ok {
-			terms = append(terms, token)
-			visited[token] = true
-		}
-	}
-	for _, token := range other.UniqueTokens {
-		if _, ok := visited[token]; !ok {
-			terms = append(terms, token)
-			visited[token] = true
-		}
-	}
-
-	vector1 := make([]float64, len(visited))
-	vector2 := make([]float64, len(visited))
-	index := 0
-	for _, term := range terms {
-		vector1[index] = d.TermFrequency(term)
-		vector2[index] = other.TermFrequency(term)
-		index += 1
-	}
-
-	return vector1, vector2
-}
-
 type Option func(idf *TfIdf)
 
 func WithStopWords(stopWords []string) Option {
@@ -93,6 +52,47 @@ func New(opts ...Option) *TfIdf {
 	return tfIdf
 }
 
+type Document struct {
+	Frequency       map[string]float64
+	UniqueTokens    []string
+	TotalTokenCount int
+}
+
+func (d Document) TermFrequency(term string) float64 {
+	if _, ok := d.Frequency[term]; !ok {
+		return 0
+	}
+	return d.Frequency[term] / (float64)(d.TotalTokenCount)
+}
+
+func (d Document) GetVectors(other Document) ([]float64, []float64) {
+	visited := make(map[string]bool, 0)
+	terms := make([]string, 0)
+	for _, token := range d.UniqueTokens {
+		if _, ok := visited[token]; !ok {
+			terms = append(terms, token)
+			visited[token] = true
+		}
+	}
+	for _, token := range other.UniqueTokens {
+		if _, ok := visited[token]; !ok {
+			terms = append(terms, token)
+			visited[token] = true
+		}
+	}
+
+	vector1 := make([]float64, len(visited))
+	vector2 := make([]float64, len(visited))
+	index := 0
+	for _, term := range terms {
+		vector1[index] = d.TermFrequency(term)
+		vector2[index] = other.TermFrequency(term)
+		index += 1
+	}
+
+	return vector1, vector2
+}
+
 type Comparator func(vector1, vector2 []float64) float64
 
 func (i TfIdf) Compare(document1, document2 string, comparator Comparator) (float64, error) {
@@ -132,6 +132,7 @@ func (i TfIdf) TermFrequencyInverseDocumentFrequency(term string, document strin
 	if doc == nil {
 		return 0
 	}
+
 	return doc.TermFrequency(term) * i.InverseDocumentFrequency(term)
 }
 
